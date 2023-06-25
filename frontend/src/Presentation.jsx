94270ee35c4a76e4ab36a94ea3eeb9f84f53a0e0
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-06-25 19:46:22 +0200

    four

--- frontend/src/Presentation.jsx
+++ frontend/src/Presentation.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
 import guy from './assets/guy.png'
 import './styles/Presentation.css'
 
 function Presentation() {
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+
+    useEffect(() => {
+
+        const handleMove = (event) => {
+            const { clientX, clientY } = event
+            setPosition({ x: clientX, y: clientY })
+        }
+
+        window.addEventListener('pointermove', handleMove)
+
+        return () => {
+            window.removeEventListener('pointermove', handleMove)
+        }
+    })
+
     return (
         <main>
             <div className="img">
@@ -10,8 +27,22 @@ function Presentation() {
             <div className="text">
                 <span>WE ARE</span>
                 <h1>CODE MATCH</h1>
-                <h3>WHERE YOU COULD FIND<br></br> YOUR WEBSITE DEVELOPER</h3>
+                <h3>WHERE YOU COULD FIND YOUR</h3>
+                <h2>WEBSITE DEVELOPER</h2>
             </div>
+            <div style={{
+                position: 'absolute',
+                background: '#5CD2C6',
+                borderRadius: '50%',
+                opacity: 1,
+                pointerEvents: 'none',
+                left: -2.5,
+                top: -2.5,
+                width: 5,
+                height: 5,
+                transform: `translate(${position.x}px, ${position.y}px)`
+            }}
+            />
         </main>
     )
 }
