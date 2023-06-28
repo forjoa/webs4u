70d9f47ae3528c08f0c9ab019bcbb4dc0b8d891f
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-06-28 20:56:00 +0200

    8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,21 @@ import Header from "./Header"
 import '../src/styles/App.css'
 import Presentation from "./Presentation"
 import Us from "./Us"
+import { motion, useScroll } from "framer-motion"
 
 function App() {
 
+  const { scrollYProgress } = useScroll();
+
   return (
     <>
-    <Header></Header>
-    <Presentation></Presentation>
-    <Us></Us>
+      <Header></Header>
+      <Presentation></Presentation>
+      <Us></Us>      
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
     </>
   )
 }
