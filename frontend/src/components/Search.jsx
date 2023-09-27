865a5669ec943af64a52771cb192e5721f8be91d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-09-27 19:46:43 +0200

    Updated some styles and added the input to search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,9 +1,16 @@
 import Header from './Header.jsx'
 
+import '../styles/Search.css'
+
 const Search = () => {
     return (
         <>
             <Header />
+            <input 
+                className='input-search'
+                type='text' 
+                spellCheck='false'
+            />
         </>
     )
 }
