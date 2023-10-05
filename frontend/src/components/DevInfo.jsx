79688802000b91039ca6b9ae17d11600b6c9ed49
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-10-05 21:53:03 +0200

    Finished desktop card for each Dev Info

--- frontend/src/components/DevInfo.jsx
+++ frontend/src/components/DevInfo.jsx
@@ -8,18 +8,26 @@ const DevInfo = ({ data, close }) => {
             <button className="close" onClick={close}>
                 &times;
             </button>
-            <h2 className="header">{data.name}</h2>
-            <div className="content">
-                {data.description}
-                <ul>
-                    {
-                        pLang.map((lang, index) => {
-                            return (
-                                <li key={index}>{lang}</li>
-                            )
-                        })
-                    }
-                </ul>
+            <div className="devinfo-content">
+                <div className="photo-name">
+                    <img src={data.photo} />
+                    <h2 className="header">{data.name}</h2>
+                </div>
+                <div className="content">
+                    <h3>Descripción</h3>
+                    <p>{data.description}</p>
+                    <h3>Lenguajes:</h3>
+                    <ul>
+                        {
+                            pLang.map((lang, index) => {
+                                return (
+                                    <li key={index}>{lang}</li>
+                                )
+                            })
+                        }
+                    </ul>
+                    <button className='contact-me-btn'>Enviar mensaje</button>
+                </div>
             </div>
         </div>
     )
