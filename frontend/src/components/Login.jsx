efe708296e384332f258d9984c4a580bf4b020aa
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-10-06 13:40:52 +0200

    Form finished

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,16 +1,20 @@
 import '../styles/Login.css'
 
+import loginIcon from '../icons/loginIcon.svg'
+
 const Login = (close) => {
     return (
         <form className='login-form'>
-            <button onClick={close}>&times;</button>
+            <button onClick={close} className='close'>&times;</button>
+            <div className="login-title">
+                <img src={loginIcon} className='login-icon' />
+                <h2>Login</h2>
+            </div>
             <label htmlFor='name'>Nombre</label>
-            <br></br>
             <input type="text" name="name" placeholder="Nombre" />
-            <br></br>
             <label htmlFor="email">Email</label>
-            <br></br>
             <input type="email" name="email" placeholder="Email" />
+            <input type='submit' className='submit-login'/>
         </form>
     )
 }
