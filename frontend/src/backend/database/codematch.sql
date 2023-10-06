aeb618dc55104e3f69a8bb2e4ea0724dcd784e6b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-10-06 22:25:14 +0200

    Contact form completly available

--- frontend/src/backend/database/codematch.sql
+++ frontend/src/backend/database/codematch.sql
@@ -12,9 +12,12 @@ CREATE TABLE developers (
     email varchar(255),
     pwd varchar(255),
     programming_lang varchar(255),
+    description text,
     photo longblob
 );
 
-insert into developers(name, email, pwd, programming_lang) values('joaquin','joaquin_trujillo@icloud.com','1234', 'React/Node/JavaScript/PHP');
-insert into developers(name, email, pwd, programming_lang) values('rebe','rebe@icloud.com','1234', 'Angular/Node/TypeScript');
-insert into developers(name, email, pwd, programming_lang) values('forky','forky@icloud.com','1234', 'HTML/CSS/JavaScript');
+insert into developers(name, email, pwd, programming_lang, description) values('joaquin','joaquin_trujillo@icloud.com','1234', 'React/Node/JavaScript/PHP', 'Lorem ipsum dolor sit amet consectetur adipiscing, elit natoque platea sagittis aliquet malesuada, penatibus duis hac facilisis auctor. Condimentum potenti volutpat parturient odio eleifend magnis nascetur mauris cum curae, inceptos erat ut nibh integer primis netus porta placerat, risus ultrices id posuere lacus dis at donec leo. Varius aliquam sodales netus vulputate diam hendrerit commodo, nullam morbi placerat blandit cursus inceptos cras, scelerisque donec volutpat potenti ultrices nulla.');
+insert into developers(name, email, pwd, programming_lang, description) values('rebe','rebe@icloud.com','1234', 'Angular/Node/TypeScript', 'Lorem ipsum dolor sit amet consectetur adipiscing, elit natoque platea sagittis aliquet malesuada, penatibus duis hac facilisis auctor. Condimentum potenti volutpat parturient odio eleifend magnis nascetur mauris cum curae, inceptos erat ut nibh integer primis netus porta placerat, risus ultrices id posuere lacus dis at donec leo. Varius aliquam sodales netus vulputate diam hendrerit commodo, nullam morbi placerat blandit cursus inceptos cras, scelerisque donec volutpat potenti ultrices nulla.');
+insert into developers(name, email, pwd, programming_lang, description) values('forky','forky@icloud.com','1234', 'HTML/CSS/JavaScript', 'Lorem ipsum dolor sit amet consectetur adipiscing, elit natoque platea sagittis aliquet malesuada, penatibus duis hac facilisis auctor. Condimentum potenti volutpat parturient odio eleifend magnis nascetur mauris cum curae, inceptos erat ut nibh integer primis netus porta placerat, risus ultrices id posuere lacus dis at donec leo. Varius aliquam sodales netus vulputate diam hendrerit commodo, nullam morbi placerat blandit cursus inceptos cras, scelerisque donec volutpat potenti ultrices nulla.');
+
+select * from doubts;
--- frontend/src/backend/server.js
+++ frontend/src/backend/server.js
@@ -1,27 +1,43 @@
+import express from "express";
+import cors from "cors";
+import db from "./database/database.js";
 
-import express from 'express'
-import cors from 'cors'
-import db from './database/database.js'
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+// show developers
+app.get("/developers", (req, res) => {
+  db.query("SELECT * FROM developers", (err, results) => {
+    if (err) {
+      console.error("error en la consulta");
+      res.status(500).send("error en la consulta");
+      return;
+    }
 
-// mostrar developers
-app.get('/developers', (req, res) => {
-    db.query('SELECT * FROM developers', (err, results) => {
-        if (err) {
-            console.error('error en la consulta')
-            res.status(500).send('error en la consulta')
-            return
-        }
+    res.json(results);
+  });
+});
 
-        res.json(results)
-    })
-})
+// insert doubts from contact section
+app.post("/doubts", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const query = "INSERT INTO doubts (name, email, message) values (?, ?, ?)";
+
+  db.query(query, [name, email, message], (err, result) => {
+    if (err) {
+      console.error("error al enviar mensaje", err);
+      res.status(500).send("error al enviar mensaje");
+      return;
+    }
+
+    res.json({ message: "mensaje enviado correctamente" });
+  });
+});
 
 app.listen(port, () => {
-    console.log(`server en port: ${port}`)
-})
+  console.log(`server en port: ${port}`);
+});
