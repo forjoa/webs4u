0b6ac718fbbd83b20108c7f06f028a4599dc8c8a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-10-06 21:42:59 +0200

    Login finished

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,8 @@ import '../styles/Login.css'
 
 import loginIcon from '../icons/loginIcon.svg'
 
+import { Link } from 'react-router-dom'
+
 const Login = (close) => {
     return (
         <form className='login-form'>
@@ -10,11 +12,16 @@ const Login = (close) => {
                 <img src={loginIcon} className='login-icon' />
                 <h2>Login</h2>
             </div>
-            <label htmlFor='name'>Nombre</label>
-            <input type="text" name="name" placeholder="Nombre" />
             <label htmlFor="email">Email</label>
             <input type="email" name="email" placeholder="Email" />
+            <label htmlFor='pwd'>Contraseña</label>
+            <input type='password' name='pwd' placeholder='******'/>
             <input type='submit' className='submit-login'/>
+
+            <div className="register-box">
+                <p className='register-title'>¿Eres un desarrollador? ¡Regístrate!</p>
+                <Link to='/register-dev' className='sign-in-btn'>Sign in</Link>
+            </div>
         </form>
     )
 }
